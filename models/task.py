d0f5f7f7bfa6cebabe66002d1fc3886894f79fe8
itsaniket2007 <itsaniket2007@gmail.com>
2026-08-18 05:22:44 +0530

    refactor: encapsulate task completion status

--- models/task.py
+++ models/task.py
@@ -1,13 +1,22 @@
-from datetime import datetime
-
-
 class Task:
 
     def __init__(self, title, priority, due_date):
         self.title = title
         self.priority = priority
         self.due_date = due_date
-        self.completed = False
+        self._completed = False
+
+    @property
+    def completed(self):
+        return self._completed
+
+    @completed.setter
+    def completed(self, value):
+
+        if not isinstance(value, bool):
+            raise ValueError("Completed status must be True or False.")
+
+        self._completed = value
 
     def mark_completed(self):
         self.completed = True
